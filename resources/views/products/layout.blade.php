c560b9ac6f222224518e1ff70bcef0002eefb2dc
naoufal elghazzar <nifal21elghazzar@gmail.com>
2024-02-28 00:06:58 +0100

    creating route/controller/model/and a blade layout

---
+++ resources/views/products/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+    <title>laravel crud app</title>
+</head>
+<body>
+    <h1>laravel crud app</h1>
+    <div class="container" >
+        <br>
+        @yield('content')
+    </div>
+
+</body>
+</html>
