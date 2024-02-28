81fd332d07ce7f3690dc70dd4c456ae19756a036
naoufal elghazzar <nifal21elghazzar@gmail.com>
2024-02-28 03:22:50 +0100

    adding the pagination link to the index.blade.php

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -16,17 +16,28 @@
     <th>id</th>
     <th>Name</th>
     <th>Details</th>
-    <th>actions</th>
+    <th width="280px" >actions</th>
 </tr>
-@foreach ( $product as $p )
+@foreach ( $product as $pr )
 <tr>
-    <td>{{ $p->id }}</td>
-    <td>{{ $p->name }}</td>
-    <td>{{ $p->details }}</td> 
+    <td>{{ $pr->id }}</td>
+    <td>{{ $pr->name }}</td>
+    <td>{{ $pr->details }}</td> 
+    <td>  
+
+   <form action="{{ route('products.destroy',$pr->id) }}" method="POST" >
+    <a class="btn btn-info" href="{{ route('products.show',$pr->id) }}">show</a> 
+    <a class="btn btn-secondary" href="{{ route('products.edit',$pr->id) }}">edit</a> 
+    @csrf
+     @method('DELETE')
+     <button type="submit" class="btn btn-danger">Delete</button>
+    </form>     
+
+    </td>
 </tr>
 @endforeach
 </table>
 
-
+{{ $product->links() }}
 
 @endsection
